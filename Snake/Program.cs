3cc97addaa8189efd9944891a6087d6c65db400c
Artem Mayer <mayer1art@gmail.com>
2022-01-17 02:24:52 +0700

    Update Program.cs - function

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,7 +6,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char symb1 = '*';
+
+            DrawSymb(x1, y1, symb1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char symb2 = '#';
+
+            DrawSymb(x2, y2, symb2);
+        }
+
+        static void DrawSymb(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
         }
     }
 }
